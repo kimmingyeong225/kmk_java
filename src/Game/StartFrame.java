fc766d5108edc9750bee5dc506ac3d1ad79e75eb
kmk <kred730036@gmail.com>
2024-05-13 12:43:12 +0900

    프로듀서_컨슈머패턴

---
+++ src/Game/StartFrame.java
@@ -0,0 +1,34 @@
+package Game;
+
+/*
+ * 타이틀, 버튼(시작, 종료, 설정)
+ */
+
+public class StartFrame {
+
+	// 생성자
+	public StartFrame() {
+		// 사이즈, 타이틀, 
+		initData();
+		setInitLayout();
+		addEventListener();
+		
+	}
+	
+	private void initData() {
+		
+	}
+	
+	private void setInitLayout() {
+		
+	}
+	
+	private void addEventListener() {
+		
+	}
+	
+	
+	
+}// end of class
+
+
